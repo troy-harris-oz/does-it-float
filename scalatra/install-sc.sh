fd0ee249b8e0cbbac0c412c395781cce64c515cf
Troy Harris <troy.harris.oz@gmail.com>
2013-11-03 17:07:48 +0000

    Fix scala version value  for Scalatra install script

--- scalatra/install-sc.sh
+++ scalatra/install-sc.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
 # Install doc - http://scalatra.org/getting-started/installation.html
 
+SCALA_VERSION=2.10.2
+ORG=com.toldknow
+NAME=scalatra-app
+PACKAGE=com.toldknow.app
+SERVLET=AppServlet
+
 curl https://raw.github.com/n8han/conscript/master/setup.sh | sh
 PATH=$PATH:~/bin
 export PATH
 source ~/.bashrc
-
 cs n8han/giter8
-g8 scalatra/scalatra-sbt --organization=com.toldknow --name=scalatra-app --servlet_name=ScalatraServlet --scala_version=2.9.2 
-
-
+g8 scalatra/scalatra-sbt --organization=$ORG --name=$NAME --servlet_name=$SERVLET --scala_version=$SCALA_VERSION
+cd scalatra-app
+chmod u+x sbt
+./sbt compile
